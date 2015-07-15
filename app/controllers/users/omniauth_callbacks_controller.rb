844c7d63bb8f3304eb16458ea646a611155eaea5
Nick Chen <ska4908@gmail.com>
2015-07-15 20:42:11 +0800

    add Access-Control-Allow-Origin for FB OAuth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -28,6 +28,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
+    headers['Access-Control-Allow-Origin'] = '*'
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
